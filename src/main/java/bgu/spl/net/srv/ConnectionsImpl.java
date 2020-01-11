bdebf52f3ff80664fca82aa541041650f43b5e47
Hadas Zeira <zeirah@post.bgu.ac.il>
2020-01-12 01:58:13 +0200

    protocol finish

--- src/main/java/bgu/spl/net/srv/ConnectionsImpl.java
+++ src/main/java/bgu/spl/net/srv/ConnectionsImpl.java
@@ -3,26 +3,25 @@ package bgu.spl.net.srv;
 import bgu.spl.net.impl.frameObjects.FrameObject;
 
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class ConnectionsImpl<T> implements Connections<T> {
 
-    private ConcurrentHashMap<String, String> users;
-    private ConcurrentHashMap<String, ConcurrentLinkedQueue<String>> genreSubscribers;
-    private ConcurrentHashMap<Integer, ConnectionHandlerImpl> handlers;
-    private ConcurrentHashMap<String, Integer> userHandlers;
+    private ConcurrentHashMap<Integer, User> users; //CHid_user
+    private ConcurrentHashMap<String, LinkedList<User>> genreSubscribers; //genre_users
+    private ConcurrentHashMap<Integer, ConnectionHandlerImpl> handlers; //????
+
 
     public ConnectionsImpl() {
         users = new ConcurrentHashMap<>();
         genreSubscribers = new ConcurrentHashMap<>();
-        userHandlers = new ConcurrentHashMap<>();
-        handlers = new ConcurrentHashMap<>();
+        handlers = new ConcurrentHashMap<>();    ////////TO ADD THE CONHAN TO THE LIST
     }
-
     @Override
     public boolean send(int connectionId, T msg) {
-        if(handlers.containsKey(connectionId)) {
+        if (handlers.containsKey(connectionId)) {
             ConnectionHandlerImpl connectionHandler = handlers.get(connectionId);
             connectionHandler.send(msg);
             return true;
@@ -32,54 +31,57 @@ public class ConnectionsImpl<T> implements Connections<T> {
 
     @Override
     public void send(String genre, T msg) {
-       // FrameObject frameObject = (FrameObject)msg;
-        if(genreSubscribers.containsKey(genre) && !genreSubscribers.get(genre).isEmpty()) {
-            ConcurrentLinkedQueue<String> subUsers = genreSubscribers.get(genre);
-            for (String s : subUsers) {
-                int connId = userHandlers.get(s);
-                send(connId, msg);
+        if (genreSubscribers.containsKey(genre) && !genreSubscribers.get(genre).isEmpty()) {
+            LinkedList<User> subUsers = genreSubscribers.get(genre);
+            for (User u : subUsers) {
+                send(u.getConnectionId(), msg);
             }
         }
     }
 
     @Override
-    public void disconnect(int connectionId) {
-        if(handlers.containsValue(connectionId)){
-            //Find the user with connectionId
-            String currUser = "";
-            for (String s : userHandlers.keySet()) {
-                if(userHandlers.get(s) == connectionId)
-                    currUser = s;
-            }
+    public void disconnect(int connectionId) { //TODO delete from subGenres, delete from handlers, close socket
+        //delete this connections handler
+       User currUser = users.get(connectionId);
+       currUser.setLogged(false);
+       currUser.getGenres().clear();
+       currUser.getIdSubscriptions().clear();
 
-            //Delete this user from topics he's subscribed to
-            for (String genre : genreSubscribers.keySet()) {
-                ConcurrentLinkedQueue<String> subUsers = genreSubscribers.get(genre);
-                for (String usr : subUsers) {
-                    if(usr.equals(currUser)) {
-                        subUsers.remove(usr);
-                        genreSubscribers.replace(genre, subUsers);
-                    }
-                }
-            }
-
-            //Close the socket
+        if (handlers.containsValue(connectionId)) {
+            handlers.remove(connectionId);
         }
+
+        //Delete this user from topics he's subscribed to
+        for (String genre : genreSubscribers.keySet()) {
+            LinkedList<User> subUsers = genreSubscribers.get(genre);
+            for (User u : subUsers) {
+                subUsers.remove(u);
+                genreSubscribers.replace(genre, subUsers);
+            }
+        }
+
+        //Close the socket
+
+
     }
 
-    public ConcurrentHashMap<String, String> getUsers() {
+
+
+
+
+    public ConcurrentHashMap<Integer, User> getUsers() {
         return users;
     }
 
-    public void setUsers(ConcurrentHashMap<String, String> users) {
+    public void setUsers(ConcurrentHashMap<Integer, User> users) {
         this.users = users;
     }
 
-    public ConcurrentHashMap<String, ConcurrentLinkedQueue<String>> getGenreSubscribers() {
+    public ConcurrentHashMap<String, LinkedList<User>> getGenreSubscribers() {
         return genreSubscribers;
     }
 
-    public void setGenreSubscribers(ConcurrentHashMap<String, ConcurrentLinkedQueue<String>> genreSubscribers) {
+    public void setGenreSubscribers(ConcurrentHashMap<String, LinkedList<User>> genreSubscribers) {
         this.genreSubscribers = genreSubscribers;
     }
 
@@ -91,11 +93,5 @@ public class ConnectionsImpl<T> implements Connections<T> {
         this.handlers = handlers;
     }
 
-    public ConcurrentHashMap<String, Integer> getUserHandlers() {
-        return userHandlers;
-    }
-
-    public void setUserHandlers(ConcurrentHashMap<String, Integer> userHandlers) {
-        this.userHandlers = userHandlers;
-    }
 }
+
